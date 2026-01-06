3be565cdf5c76d11f871540af86d1f5c5a3f9b45
Matheus Bonato Veloso Batista <bqnato@gmail.com>
2026-01-06 19:18:51 -0300

    Projeto criado

---
+++ src/main/java/dev/matheus/sistemarh/SIstemaRhApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus.sistemarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIstemaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIstemaRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheus/sistemarh/SIstemaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheus.sistemarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SIstemaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
